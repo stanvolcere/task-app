6c09bdb79f8ea873428fee74728f885226e145c6
stanvolcere <stanvolcere@hotmail.com>
2019-03-29 18:06:09 +0000

    added welcome message on index page

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
+app.get("/", (req, res) => {
+    res.send({
+        Message: "Welcome to the Task App API. Please sign up or log in!"
+    });
+});
+
 app.listen(port, () => {
     console.log("Server has started on port " + port);
 });
